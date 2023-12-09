8c8edbd9ef1470f6a6f3a1b18bf6ae9922084b91
shreyansh <shreyansh2118tiwari@gmail.com>
2023-12-09 17:09:07 +0530

    second

--- lib/add item.dart
+++ lib/add item.dart
@@ -1,139 +1,10 @@
 import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ItemDetails {
-  String itemName;
-  DateTime installmentDate;
-  DateTime maintenanceDate;
-  File? image;
-  String contractorContact;
-
-  ItemDetails({
-    required this.itemName,
-    required this.installmentDate,
-    required this.maintenanceDate,
-    this.image,
-    required this.contractorContact,
-  });
-}
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Item Details',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: DisplayItemScreen(),
-    );
-  }
-}
-
-class DisplayItemScreen extends StatefulWidget {
-  @override
-  _DisplayItemScreenState createState() => _DisplayItemScreenState();
-}
-
-class _DisplayItemScreenState extends State<DisplayItemScreen> {
-  List<ItemDetails> _itemList = [];
-
-  int _calculateMonthsDifference(DateTime startDate, DateTime endDate) {
-    final difference = endDate.difference(startDate).inDays;
-    return (difference / 30).ceil(); // Rounded up to the nearest month
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Item Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            TextButton(
-              onPressed: () async {
-                final result = await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AddItemScreen()),
-                );
-                if (result != null) {
-                  setState(() {
-                    _itemList.add(result as ItemDetails);
-                  });
-                }
-              },
-              child: Text(
-                'Add Item',
-                style: TextStyle(color: Colors.white),
-              ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(Colors.blue),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            if (_itemList.isNotEmpty)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _itemList.length,
-                  itemBuilder: (context, index) {
-                    final maintenanceDifference = _calculateMonthsDifference(
-                      _itemList[index].maintenanceDate,
-                      DateTime.now(),
-                    );
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        if (_itemList[index].image != null)
-                          Container(
-                            height: 230,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8.0),
-                              image: DecorationImage(
-                                image: FileImage(_itemList[index].image!),
-                                fit: BoxFit.cover,
-                              ),
-                            ),
-                          ),
-
-                        Row(
-                          children: [
-                            Text(
-                              '${_itemList[index].itemName}: ',
-                              style: TextStyle(fontWeight: FontWeight.w600),
-                            ),
-                            Text(
-                              ' $maintenanceDifference months',
-                            ),
-                          ],
-                        ),
-                        Divider(),
-                      ],
-                    );
-                  },
-                ),
-              ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
+import 'itemscreen.dart';
 class AddItemScreen extends StatefulWidget {
   @override
   _AddItemScreenState createState() => _AddItemScreenState();
@@ -191,9 +62,9 @@ class _AddItemScreenState extends State<AddItemScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Add Item'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Add Item'),
+      // ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
@@ -201,6 +72,13 @@ class _AddItemScreenState extends State<AddItemScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
+              SizedBox(height: 30,),
+              Center(
+                child: Container(
+                  child: Text("Add item",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 32),),
+                ),
+              ),
+              SizedBox(height: 30,),
               TextFormField(
                 decoration: InputDecoration(labelText: 'Item Name'),
                 onChanged: (value) {
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import 'homePage.dart';
+import 'itemscreen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Google Sign-In',
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -58,24 +59,58 @@ class _GoogleSignInScreenState extends State<GoogleSignInScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Google Sign-In'),
-      ),
+      // appBar: AppBar(
+      //   title: Text('Google Sign-In'),
+      // ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            User? user = await _signInWithGoogle();
-            if (user != null) {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => DisplayItemScreen()),
-              );
-              // Navigate to the next screen or perform actions after sign-in
-              print('User signed in: ${user.displayName}');
-            }
-          },
-          child: Text('Sign in with Google'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              child: Text("Sign up/ Sign in",style: TextStyle(fontWeight: FontWeight.w700,fontSize: 45),),
+            ),
+            SizedBox(height: 30,),
+            Container(
+              width: 250,
+              height: 45,
+              child: ElevatedButton(
+                onPressed: () async {
+                  User? user = await _signInWithGoogle();
+                  if (user != null) {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => DisplayItemScreen()),
+                    );
+                    // Navigate to the next screen or perform actions after sign-in
+                    print('User signed in: ${user.displayName}');
+                  }
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'asset/googleImg.jpg', // Replace with your Google icon asset path
+                      height: 24, // Adjust the height as needed
+                      width: 24, // Adjust the width as needed
+                    ),
+                    SizedBox(width: 8),
+                    Text(
+                      'Continue with Google',
+                      style: TextStyle(
+                        color: Colors.black, // Change the text color as needed
+                        fontSize: 16, // Adjust the font size as needed
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
+
       ),
     );
   }
